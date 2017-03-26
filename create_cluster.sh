cb5e8ccc2bacc0fd42ae54b024fc358f2ed9f67f
Shuichi TSUKAMOTO <shuichi.tsukamoto@microsoft.com>
2017-03-27 07:56:19 +0900

    update create vm

--- create_cluster.sh
+++ create_cluster.sh
@@ -30,12 +30,12 @@ location=${3}
 
 i=0
 while [ ${i} -lt ${1} ]; do
-    if [ ${i} -lt 3 ]; then
+    if [ ${i} -lt 1 ]; then
         echo "${i}"
-        /usr/bin/nohup /usr/local/bin/ansible-playbook --extra-vars "no=-${i} host_group=cdh-hadoop nic_name=nic-centos73-${2}-${randstr} public_ip_name=public-ip-centos73-${2}-${randstr} vm_name=vm-centos73-${2}-${randstr} security_group_name=${security_group_name} storage_account_name=kos${2}${randstr}${i} vm_network_name=${vm_network_name} resource_group=${resource_group} subnet_name=${vm_network_sub_name} location=${location} vm_size=Standard_DS2_v2 storage_accout_type=Standard_LRS storage_kind=Storage" ${yaml_path}/create_vm_hadoop_mapreduce.yml -vvv > ${yaml_path}/nohup_std.out.${i} 2>&1 &
+        /usr/bin/nohup /usr/local/bin/ansible-playbook --extra-vars "no=-${i} host_group=cdh-hadoop nic_name=nic-centos73-${2}-${randstr} public_ip_name=public-ip-centos73-${2}-${randstr} vm_name=vm-centos73-${2}-${randstr} security_group_name=${security_group_name} storage_account_name=kos${2}${randstr}${i} vm_network_name=${vm_network_name} resource_group=${resource_group} subnet_name=${vm_network_sub_name} location=${location} vm_size=Standard_DS2_v2 storage_accout_type=Standard_LRS storage_kind=Storage domain_name_label=vm-centos73-${2}-${randstr}-${i}" ${yaml_path}/create_vm_ambari.yml -vvv > ${yaml_path}/nohup_std.out.${i} 2>&1 &
     else
         echo "${i}"
-        /usr/bin/nohup /usr/local/bin/ansible-playbook --extra-vars "no=-${i} host_group=cdh-hadoop nic_name=nic-centos73-${2}-${randstr} public_ip_name=public-ip-centos73-${2}-${randstr} vm_name=vm-centos73-${2}-${randstr} security_group_name=${security_group_name} storage_account_name=kos${2}${randstr}${i} vm_network_name=${vm_network_name} resource_group=${resource_group} subnet_name=${vm_network_sub_name} location=${location} vm_size=Standard_DS2_v2 storage_accout_type=Standard_LRS storage_kind=Storage" ${yaml_path}/create_vm_hadoop_datanode.yml -vvv > ${yaml_path}/nohup_std.out.${i} 2>&1 &
+        /usr/bin/nohup /usr/local/bin/ansible-playbook --extra-vars "no=-${i} host_group=cdh-hadoop nic_name=nic-centos73-${2}-${randstr} public_ip_name=public-ip-centos73-${2}-${randstr} vm_name=vm-centos73-${2}-${randstr} security_group_name=${security_group_name} storage_account_name=kos${2}${randstr}${i} vm_network_name=${vm_network_name} resource_group=${resource_group} subnet_name=${vm_network_sub_name} location=${location} vm_size=Standard_DS2_v2 storage_accout_type=Standard_LRS storage_kind=Storage domain_name_label=vm-centos73-${2}-${randstr}-${i}" ${yaml_path}/create_vm_hadoop_datanode.yml -vvv > ${yaml_path}/nohup_std.out.${i} 2>&1 &
     fi
     i=`expr ${i} + 1`
 done
